de63457f4a2ea97e110f8ac84d9f46676ce3a93d
maringan <kamil.kucharski88+1@gmail.com>
2014-05-25 19:46:28 +0200

    refactor

--- lib/parse/conf/gitosis_style.rb
+++ lib/parse/conf/gitosis_style.rb
@@ -36,15 +36,15 @@ module Parse
             group_name = Helper.trim_and_remove_ugly_symbol(line)
             data[group_name] = [] 
           else
-            item = line.split("=").map{ |it| Helper.trim_and_remove_ugly_symbol(it) }
-            key, value = item[0], item[1].split(@separator)
-            
-            insert_etntry = {key => value}
             insert_key = group_name ? group_name : empty_group_name
-            
-            data[insert_key].push(insert_etntry)
+            data[insert_key].push(build_line_insert_entity(line))
           end          
         end
+      end
+
+      def build_line_insert_entity(line)
+        item = line.split('=').map{ |it| Helper.trim_and_remove_ugly_symbol(it) }
+        Hash[*[item[0], item[1].split(@separator)]]
       end
     end
   end
